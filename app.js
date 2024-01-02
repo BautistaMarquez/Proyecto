326512336c870e68174acb434b6a601ac1137fd1
BautistaMarquez <bau.marquez@hotmail.com>
2024-01-02 21:02:12 +0100

    asdaq

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /*variables de las secciones*/
 const section1 = document.querySelectorAll('.seccion1');
 const section3 = document.querySelectorAll('.seccion3');
+const section6 = document.querySelectorAll('.seccion6');
 /*observador de las secciones*/
 
 const observador1 = new IntersectionObserver((entries)=>{
@@ -8,7 +9,7 @@ const observador1 = new IntersectionObserver((entries)=>{
         entry.target.classList.toggle('aparecer1', entry.isIntersecting);
     });
 },
-    {threshold: 0.7}
+    {threshold: [0.2,0.8]}
 );
 
 section1.forEach((seccion)=> observador1.observe(seccion));
@@ -19,10 +20,18 @@ const observador3 = new IntersectionObserver((entries)=>{
         entry.target.classList.toggle('aparecer3', entry.isIntersecting);
     });
 },
-    {threshold: 0.5}
+{threshold: [0.2,0.8]}
 );
 
 section3.forEach((seccion)=> observador3.observe(seccion));
 
+const observador6 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        entry.target.classList.toggle('aparecer6', entry.isIntersecting);
+    });
+},
+{threshold: [0.2,0.8]}
+);
 
+section6.forEach((seccion)=> observador6.observe(seccion));
 
